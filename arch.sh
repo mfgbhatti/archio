d36135f21bb2bc7a5f22ad317da3b1d53883af9d
farhan ghani <13787498+mfgbhatti@users.noreply.github.com>
2022-04-18 00:35:59 +0100

    fixes and updates

--- arch.sh
+++ arch.sh
@@ -19,7 +19,16 @@
 # Function
 installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
+# Variables
+MOUNTPOINT="/mnt"
+BTRFS_SUBVOLUMES=(@ @opt @tmp @var @usr-local)
+MOUNTOPTION="noatime,commit=120,compress=zstd,ssd"
+BOOT_PARTITION="/dev/nvme0n1p1"
+ROOT_PARTITION="/dev/nvme0n1p2"
+
+# starting ...
 timedatectl set-ntp true
+loadkeys uk
 sed -i "s/^#Para/Para/;s/^#Color$/Color/" /etc/pacman.conf
 for x in archlinux-keyring reflector rsync; do
 	installpkg "$x"
@@ -27,18 +36,27 @@ done
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.back
 reflector --age 48 --country GB --fastest 5 --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy --noconfirm
-mkfs.vfat -F 32 -n "EFI" /dev/nvme0n1p1
-mkfs.btrfs -L ROOT /dev/nvme0n1p2 -f
-mount -L ROOT /mnt
-btrfs subvolume create /mnt/@
-umount /mnt
-btrfs check --clear-space-cache v2 /dev/nvme0n1p2
-mount -o noatime,commit=120,compress=zstd,ssd,subvol=@ -L ROOT /mnt
-mkdir /mnt/{boot,home}
-mount -t vfat -L EFI /mnt/boot
-mount -t btrfs -L HOME /mnt/home
-pacstrap /mnt base base-devel linux linux-firmware linux-headers intel-ucode neovim nano sudo archlinux-keyring wget --noconfirm --needed
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-cp /etc/pacman.conf /mnt/etc/pacman.conf
-genfstab -U /mnt >> /mnt/etc/fstab
-curl https://raw.githubusercontent.com/mfgbhatti/archio/main/chroot.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
+mkfs.vfat -F 32 -n "EFI" "$BOOT_PARTITION"
+mkfs.btrfs -L ROOT "$ROOT_PARTITION" -f
+mount "$ROOT_PARTITION" "$MOUNTPOINT"
+echo "Creating subvolumes and directories"
+for x in "${BTRFS_SUBVOLUMES[@]}"; do
+	btrfs subvolume create "$MOUNTPOINT"/"${x}" >/dev/null 2>&1
+done
+umount "$MOUNTPOINT"
+btrfs check --clear-space-cache v2 "$ROOT_PARTITION"
+mount -o "$MOUNTOPTION",subvol=@ "$ROOT_PARTITION" "$MOUNTPOINT"
+for VOL in "${BTRFS_SUBVOLUMES[@]:1}"; do
+	DIR="${VOL//@/}"
+	DIR=$(echo "$DIR" | sed 's/-/\//')
+	mkdir -p "$MOUNTPOINT"/"$DIR"
+	mount -o "$MOUNTOPTION",subvol="$VOL" "$ROOT_PARTITION" "$MOUNTPOINT"/"$DIR"
+done
+mkdir "$MOUNTPOINT"/{boot,home}
+mount -t vfat -L EFI "$MOUNTPOINT"/boot
+mount -t btrfs -L HOME "$MOUNTPOINT"/home
+pacstrap "$MOUNTPOINT" base base-devel linux linux-firmware linux-headers intel-ucode neovim sudo archlinux-keyring wget --noconfirm --needed
+cp /etc/pacman.d/mirrorlist "$MOUNTPOINT"/etc/pacman.d/mirrorlist
+cp /etc/pacman.conf "$MOUNTPOINT"/etc/pacman.conf
+genfstab -U "$MOUNTPOINT" >> "$MOUNTPOINT"/etc/fstab
+curl https://raw.githubusercontent.com/mfgbhatti/archio/main/chroot.sh > "$MOUNTPOINT"/home/farhan/chroot.sh && arch-chroot "$MOUNTPOINT" /usr/bin/runuser -u farhan -- /home/farhan/chroot.sh && rm "$MOUNTPOINT"/home/farhan/chroot.sh
--- chroot.sh
+++ chroot.sh
@@ -25,11 +25,14 @@ localectl --no-ask-password set-locale LANG="en_GB.UTF-8" LC_TIME="en_GB.UTF-8"
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j8\"/;s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/" /etc/makepkg.conf
 sed -i 's/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
 echo "KEYMAP=uk" > /etc/vconsole.conf
 echo "Arch" > /etc/hostname
-PKGS = (
+PKGS=(
 'mesa'
 'xorg'
+'alacritty'
+'cups'
 'gnome-autoar'
 'gnome-backgrounds'
 'gnome-bluetooth'
@@ -63,7 +66,6 @@ PKGS = (
 'neofetch'
 'networkmanager'
 'dhclient'
-'pfetch'
 'zsh'
 'zsh-autosuggestions'
 'zsh-completions'
@@ -73,21 +75,27 @@ PKGS = (
 for PKG in "${PKGS[@]}"; do
 	installpkg "${PKG}"
 done
+
 systemctl enable NetworkManager
+systemctl enable gdm.service
+systemctl enable cups.service
+
 sed -i 's/MODULES()/MODULES(btrfs)/' /etc/mkinitcpio.conf
 mkinitcpio -P linux
 groupadd libvirt
 useradd -G wheel,libvirt -s /bin/bash farhan
 echo "farhan:1234" | chpasswd
-cd /home/farhan/
+
+cd /home/farhan/ || exit 0
 git clone "https://aur.archlinux.org/yay.git"
-cd ~/yay
+cd ~/yay || exit 0
 makepkg -si --noconfirm
-cd ~
+cd "$HOME" || exit 0
+
 AURS=(
 'arc-gtk-theme'
 'brave-bin'
-'papirus-icon-theme'
+'pfetch'
 'timeshift'
 'timeshift-autosnap'
 'visual-studio-code-bin'
@@ -96,3 +104,7 @@ AURS=(
 for AUR in "${AURS[@]}"; do
 	aurinstall "${AUR}"
 done
+
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch" --loader '/vmlinuz-linux' --unicode 'root=PARTUUID=c7ce4b26-952d-475f-84bb-44a4e5441435 rw rootflags=subvol=@ initrd=\intel-ucode.img initrd=\initramfs-linux.img'
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch-Fallback" --loader '/vmlinuz-linux' --unicode 'root=PARTUUID=c7ce4b26-952d-475f-84bb-44a4e5441435 rw rootflags=subvol=@ initrd=\intel-ucode.img initrd=\initramfs-linux-fallback.img'
+
